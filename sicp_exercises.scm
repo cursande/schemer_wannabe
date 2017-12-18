75f6dc4bc0d6e66e920a8b68646aae9bb6856857
Alex-Farries <adfarries@gmail.com>
2017-12-18 23:11:45 +1100

    sicp_ex init

---
+++ sicp_exercises.scm
@@ -0,0 +1,9 @@
+;; I'll organise these notes better once I've built them up a bit
+
+;; Procedure Definitions
+
+; General form of a procedure def: (define (<name> <formal parameters>) <body>)
+
+; Squaring ->
+(define (square x) (* x x))
+; (square 5) -> 25
