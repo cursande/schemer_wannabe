5348073e47bbc2d0251de03537aeff93aea9b9f4
Alex-Farries <adfarries@gmail.com>
2017-12-19 21:52:08 +1100

    Add notes about substitution model... * By the end of the chapter this should be a bit more organised

--- procedure_definitions.scm
+++ procedure_definitions.scm
@@ -1,9 +1,21 @@
-;; I'll organise these notes better once I've built them up a bit
-
 ;; Procedure Definitions
 
-; General form of a procedure def: (define (<name> <formal parameters>) <body>)
+;; General form of a procedure def: (define (<name> <formal parameters>) <body>)
 
-; Squaring ->
+;; Compound procedures are used in the exact same way as primitive procedures (+, -,...)
+
+;; Squaring ->
 (define (square x) (* x x))
-; (square 5) -> 25
+   
+;; (square 5) -> 25
+;; (square (+ 3 5)) -> 64
+
+;; Sum-of-squares (nested procedure) -> 
+(define (sum-of-squares x y) ; should this be in above list??
+  (+ (square x) (square y)))
+
+;; (sum-of-squares 3 5) -> 34
+
+;; Substitution model:
+;;	- We need to evaluate the operator to apply the procedure
+;;	- We need to evaluate the operands to get the arguments
---
+++ readme.md
@@ -0,0 +1,13 @@
+remote repo for examples/key notes
+
+To run scheme files in repl:
+``` shell
+scheme file.scm
+```
+
+To execute a scheme script:
+``` shell
+scheme <file.scm
+```
+
+Can also add '--quiet' to reduce output to only the program, no repl
