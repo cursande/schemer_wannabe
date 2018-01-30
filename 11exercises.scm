64c27b3d8e4afbd3ce1a09b1e335da03cda00626
Alex-Farries <adfarries@gmail.com>
2018-01-30 18:53:13 +1100

    Add 1.10

--- 11exercises.scm
+++ 11exercises.scm
@@ -93,4 +93,19 @@
 
 ; The first is recursive, the second is iterative as the variables are simply updated state to state. If I stop the process at some point in the middle, I can simply continue it by running the process with the variables saved at that last point in time.
 
+;; *1.10*
 
+(define (A x y)
+  (cond ((= y 0) 0)
+	((= x 0) (* 2 y))
+	((= y 1) 2)
+	(else (A (- x 1)
+		 (A x (- y 1))))))
+
+(A 1 10) ; -> 1024
+(A 2 4)  ; -> 65536
+(A 3 3)  ; -> 65536
+
+(define (f n) (A 0 n)) ; -> 2n
+(define (g n) (A 1 n)) ; -> 2^n
+(define (h n) (A 2 n)) ; -> 2^n^n...however long n is, minus 1 
