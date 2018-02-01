b2090ee074ef5831d4a107aba3454e19ffe1384a
Alex-Farries <adfarries@gmail.com>
2018-02-01 23:02:55 +1100

    Add actual mathematical definition for (h n)

--- 11exercises.scm
+++ 11exercises.scm
@@ -108,4 +108,4 @@
 
 (define (f n) (A 0 n)) ; -> 2n
 (define (g n) (A 1 n)) ; -> 2^n
-(define (h n) (A 2 n)) ; -> 2^n^n...however long n is, minus 1 
+(define (h n) (A 2 n)) ; -> 2^n^n...however long n is, minus 1 (*more concise  way -> 2^h(n-1))
