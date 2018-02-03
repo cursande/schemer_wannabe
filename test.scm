e735fc3552944251e1ad155314a83c8d3e0db0cd
Alex-Farries <adfarries@gmail.com>
2018-02-03 20:15:56 +1100

    Add function for finding ways to make a $

--- test.scm
+++ test.scm
@@ -1,28 +1,26 @@
 
-(define (square x) (* x x))
+; ammount: number of ways to make a dollar
+(define (count-change amount)
+  (cc amount 5))
 
-(define (cbrt-iter guess old-guess x)
-  (if (good-enough-improved guess old-guess)
-      guess
-      (cbrt-iter (improve guess x)
-                 guess
-                 x)))
+; kinds-of-coins: different coins and their values
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
 
-(define (improve guess x)
-  (/ (+ (/ x (square guess)) (* 2 guess))
-     3))
+; first-denomination: proc that takes in the number of coins still availabe, and returns the first coin that's still available
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+                               
 
-(define (average x y)
-  (/ (+ x y) 2))
-
-(define (good-enough-improved guess old-guess) ; check if difference between guesses is a tiny fraction of the guess
-  (< (abs (- old-guess guess))
-     (/ guess 100000)))
-
-(define (cbrt x)
-  (cbrt-iter 1.0 2.0 x))
-
-;(sqrt 0.000001)
-
-(cbrt 1000000000000)
+(count-change 100)
 
