ae19b8ba9106f260d45437e155c344fe26bb7b26
Alex-Farries <adfarries@gmail.com>
2018-02-17 18:08:11 +1100

    Finally figure out iterative solution, fix missing parens in former

--- 11exercises.scm
+++ 11exercises.scm
@@ -121,7 +121,13 @@
 (define (f n)
   (cond ((< n 3) n)
 	(else (+ (f (- n 1))
-		 (* 2 (f (- n 2))
-		 (* 3 (f (- n 3))))))))
+		 (* 2 (f (- n 2)))
+		 (* 3 (f (- n 3)))))))
 
+; iterative
+define (f n)
+  (f-iter n 0 1 2)) ; initialise state with these variables
 
+(define (f-iter i x y z) 
+     (cond ((= i 0) x) ; use i as counter to track loops
+	   (else (f-iter (- i 1) y z (+ z (* 2 y) (* 3 x)))))) ; just one recursive call, passing in the updated values each time
--- test.scm
+++ test.scm
@@ -4,9 +4,19 @@
 ;        { f(n - 1) + 2f(n - 2) + 3f(n - 3)     if n >= 3) }
 
 
+(define (f n)
+    (cond ((< n 3) n)
+	 (else (+ (f (- n 1))
+	          (* 2 (f (- n 2)))
+	          (* 3 (f (- n 3)))))))
 
- (define (f n)
-  (cond ((< n 3) n)
-	(else (+ (f (- n 1))
-		 (* 2 (f (- n 2))
-		 (* 3 (f (- n 3))))))))
+;--------------
+
+define (f n)
+  (f-iter n 0 1 2))
+
+(define (f-iter i x y z) 
+   (cond ((= i 0) x) 
+	 (else (f-iter (- i 1) y z (+ z (* 2 y) (* 3 x)))))) 
+
+
