0e62b70c8b9d364e67957616d87a6e587084c1e7
Alex-Farries <adfarries@gmail.com>
2018-02-24 13:52:19 +1100

    Add rec solution for 1.12

--- 11exercises.scm
+++ 11exercises.scm
@@ -134,3 +134,14 @@
 			 y 
 			 z 
 			 (+ z (* 2 y) (* 3 x)))))) 
+
+;; *1.12*
+
+; recursive
+
+(define (pascal r c) ; r = rows, c = col
+  (cond ((= c 1) 1) 
+	((= c r) 1) ; first col always 1, final column (equal to current row) always 1 as well
+	(else (+ (pascal (- r 1) (- c 1))
+		 (pascal (- r 1) c))))) ; like fib, work back recursively to the beginning, and then sub the right values in
+
--- test.scm
+++ test.scm
@@ -1,4 +1,12 @@
 
-; Pascal's triangle
+; Pascal's triangle / binomial coefficients
+
+; recursive process 
+
+(define (pascal r c) ; r = rows, c = col
+  (cond ((= c 1) 1) 
+	((= c r) 1) ; first col always 1, final column (equal to current row) always 1 as well
+	(else (+ (pascal (- r 1) (- c 1))
+		 (pascal (- r 1) c))))) ; like fib, work back recursively to the beginning, and then sub the right values in
 
 
