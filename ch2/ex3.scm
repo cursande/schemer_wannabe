379472e19874257c47790ec3e7c2f4dd83d76c0b
Alex F <adfarries@gmail.com>
2018-09-15 21:37:56 +1000

    Add second implementation for ex3

--- ch2/ex3.scm
+++ ch2/ex3.scm
@@ -27,15 +27,16 @@
   (display ")")
   p)
 
-; a rectangle is 2 sets of parallel line segments. If we find the difference between the 2 x's for the width, and
-; the difference between the 2 y's for the height, we'll have the dimensions of the rectangle. We can create the 3rd
-; and 4th points with the data available.
-
 (define (perimeter rectangle)
   (* (+ (car rectangle) (cdr rectangle)) 2))
 
 (define (area rectangle)
   (* (car rectangle) (cdr rectangle)))
+
+; 1st
+; a rectangle is 2 sets of parallel line segments. If we find the difference between the 2 x's for the width, and
+; the difference between the 2 y's for the height, we'll have the dimensions of the rectangle. We can create the 3rd
+; and 4th points with the data available.
 
 (define (rectangle-height-and-width p1 p2)
   (cons (rect-width p1 p2) (rect-height p1 p2)))
@@ -51,3 +52,21 @@
 (define test-rectangle (rectangle-height-and-width first-segment second-segment)) ; => (2 . 1)
 (perimeter test-rectangle) ; => 6
 (area test-rectangle) ; => 2
+
+; 2nd
+; We could have four points instead, and simply build the rectangle with the point differences. We have to assume the order of the points to an extent, and that a lower level of abstraction has taken care of that.
+(define (make-point x y) (cons x y))
+(define first-point (make-point 3 3))
+(define second-point (make-point 3 5))
+(define third-point (make-point 5 5))
+(define fourth-point (make-point 5 3))
+
+(define (rectangle-by-point-difference p1 p2 p3 p4)
+  (define (x-diff p1 p2) (abs (- (car p1) (car p2))))
+  (define (y-diff p1 p2) (abs (- (cdr p1) (cdr p2))))
+  (cons (x-diff p1 p3) (y-diff p2 p4)))
+
+(define rectangle (rectangle-by-point-difference first-point second-point third-point fourth-point)) ; => (2 . 2)
+
+(perimeter rectangle) ; => 8
+(area rectangle) ; => 4
