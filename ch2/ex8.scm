bc8d513e5a68fefaa7a1dc0c8918c03a0887f369
Alex F <adfarries@gmail.com>
2018-10-06 22:47:39 +1000

    Add solution for ch8

---
+++ ch2/ex8.scm
@@ -0,0 +1,21 @@
+;; Exercise 2.8. Using reasoning analogous to Alyssa’s, describe how the difference of two intervals
+;; may be computed. Define a corresponding subtraction procedure, called sub-interval.
+
+(define (make-interval a b) (cons a b))
+(define (upper-bound interval) (car interval))
+(define (lower-bound interval) (cdr interval))
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+; lower-bound for difference will be the lower bound - upper-bound,
+; the upper-bound will be the opposite, representing the greatest possible difference
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define interval-one (make-interval 4 5))
+(define interval-two (make-interval 5 8))
+
+(sub-interval interval-one interval-two) ; => (0 . -4)
