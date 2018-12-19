eb9e1fbe7cd68b501b20e9f1ff0285951cdec220
Alex F <adfarries@gmail.com>
2018-12-20 10:54:23 +1100

    Add ex.23

---
+++ ch2/ex23.scm
@@ -0,0 +1,30 @@
+;; Exercise 2.23. The procedure for-each is similar to map. It takes as arguments a procedure and a
+;; list of elements. However, rather than forming a list of the results, for-each just applies the
+;; procedure to each of the elements in turn, from left to right. The values returned by applying the
+;; procedure to the elements are not used at all -- for-each is used with procedures that perform an
+;; action, such as printing. For example,
+
+;; (for-each (lambda (x) (newline) (display x))
+;;           (list 57 321 88))
+
+;; 57
+;; 321
+;; 88
+
+;; The value returned by the call to for-each (not illustrated above) can be something arbitrary, such
+;; as true. Give an implementation of for-each.
+
+(define (for-each procedure list)
+  (define (for-each-iter remaining-values)
+    (if (null? remaining-values)
+        '()
+        (begin
+          (procedure (car remaining-values))
+          (for-each-iter (cdr remaining-values)))))
+  (for-each-iter list))
+
+(for-each (lambda (x) (newline) (display x))
+          (list 57 321 88))
+
+(for-each (lambda (x) (newline) (display (> x 0)))
+          (list 57 321 88))
