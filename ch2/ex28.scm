8374f0bf833ba0429a94884ad0e86e3dcf4be148
Alex F <adfarries@gmail.com>
2019-01-12 20:01:56 +1100

    Add 2.28

---
+++ ch2/ex28.scm
@@ -0,0 +1,24 @@
+;; Exercise 2.28. Write a procedure fringe that takes as argument a tree (represented as a list) and
+;; returns a list whose elements are all the leaves of the tree arranged in left-to-right order. For example,
+
+;; (define x (list (list 1 2) (list 3 4)))
+;; (fringe x)
+;; (1 2 3 4)
+;; (fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+
+(define x (list (list 1 2) (list 3 4)))
+x
+
+(define (fringe tree)
+  (define (fringe-iter tree result)
+    (cond ((null? tree)
+           result)
+          ((pair? (car tree))
+           (fringe-iter (cdr tree) (append result
+                                           (fringe-iter (car tree) '()))))
+          (else (fringe-iter (cdr tree) (append result
+                                                (list (car tree)))))))
+  (fringe-iter tree '()))
+
+(fringe x) ; => (1 2 3 4)
