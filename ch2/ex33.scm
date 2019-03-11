83b7fdc9c01418fcfdd3b91f963359054a37ec61
Alex F <adfarries@gmail.com>
2019-03-11 16:05:13 +1100

    Add map for 2.33

---
+++ ch2/ex33.scm
@@ -0,0 +1,27 @@
+;; Exercise 2.33. Fill in the missing expressions to complete the following definitions of some basic
+;; list-manipulation operations as accumulations:
+
+;; (define (map p sequence)
+;;   (accumulate (lambda (x y) <??>) nil sequence))
+
+;; (define (append seq1 seq2)
+;;   (accumulate cons <??> <??>))
+
+;; (define (length sequence)
+;;   (accumulate <??> 0 sequence))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y)
+                (cons (p x) y))
+              '()
+              sequence))
+
+(define (cube x) (* x x x))
+
+(map cube '(1 2 3)) ; => (1 8 27)
