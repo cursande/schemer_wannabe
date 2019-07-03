f8ad53d2442233dbaa1cbb7a861a91144f7b85e1
Alex F <adfarries@gmail.com>
2019-07-03 17:31:44 +1000

    Add 2.39b

--- ch2/ex39.scm
+++ ch2/ex39.scm
@@ -6,9 +6,24 @@
 ;; (define (reverse sequence)
 ;; (fold-left (lambda (x y) <??>) nil sequence))
 
-(define (reverse sequence)
+(define (right-reverse sequence)
   (fold-right (lambda (x y) (append y (list x)))
               '()
               sequence))
 
-(reverse (list 1 2 3)) ; => (3 2 1)
+(right-reverse (list 1 2 3)) ; => (3 2 1)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (left-reverse sequence)
+  (fold-left (lambda (x y) (cons y x))
+             '()
+             sequence))
+
+(left-reverse (list 1 2 3)) ; => (3 2 1)
