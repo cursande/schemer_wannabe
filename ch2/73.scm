7c50022b5e7394a3b0f0f9927da0875d947feeb7
Alex F <adfarries@gmail.com>
2020-04-06 15:42:42 +1000

    Finish 2.73

--- ch2/73.scm
+++ ch2/73.scm
@@ -37,8 +37,79 @@
 ;; b. Write the procedures for derivatives of sums and products, and the auxiliary code required to install
 ;; them in the table used by the program above.
 
-;; c. Choose any additional differentiation rule that you like, such as the one for exponents
-;; (exercise 2.56), and install it in this data-directed system.
+;; Without implementing 'get' and 'put', below is totally theoretical and untested:
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (install-sum-package)
+  (define (addend operands) (cadr operands))
+
+  (define (augend operands) (caddr operands))
+
+  (define (deriv operands var)
+    (make-sum (deriv (addend operands) var)
+              (deriv (addend operands) var)))
+
+  (put 'deriv '+ deriv)
+  'done)
+
+(define (install-product-package)
+  (define (multiplier operands) (cadr operands))
+
+  (define (multiplicand operands) (caddr operands))
+
+  (define (deriv operands var)
+    (make-sum
+     (make-product (multiplier operands)
+                   (deriv (multiplicand operands) var))
+     (make-product (deriv (multiplier operands) var)
+                   (multiplicand operands))))
+
+  (put 'deriv '* deriv)
+  'done)
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp)
+               var))))
+
+;; c. Choose any additional differentiation rule that you like, such as the one for exponents (exercise 2.56),
+;; and install it in this data-directed system.
+
+(define (install-exponentiation-package)
+  (define (base operands) (cadr operands))
+
+  (define (exponent operands) (caddr operands))
+
+  (define (make-exponentiation base exponent)
+    (cond ((=number? exponent 0) 1)
+          ((=number? exponent 1) base)
+          ((and (number? base) (number? exponent))
+           (expt base exponent))
+          (else (list '** base exponent))))
+
+  (define (deriv operands var)
+    (make-product (exponent operands)
+                  (make-exponentiation (base operands)
+                                       (make-sum (exponent operands) -1))))
+
+  (put 'deriv '** deriv)
+  'done)
 
 ;; d. In this simple algebraic manipulator the type of an expression is the algebraic operator that binds it
 ;; together. Suppose, however, we indexed the procedures in the opposite way, so that the dispatch line
@@ -47,3 +118,5 @@
 ;; ((get (operator exp) ’deriv) (operands exp) var)
 
 ;; What corresponding changes to the derivative system are required?
+
+;; If they're indexed with the operator/<type> first, We'd need to change the way we 'put' so that the <type> comes before the <op>
